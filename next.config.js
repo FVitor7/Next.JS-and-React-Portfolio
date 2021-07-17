52a081cecc61b905132df0c52ec1724c7bfd529c
Fábio Vitor <fabvitor2010@gmail.com>
2021-07-16 23:51:10 -0300

    test remove next/image code to project

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-const withImages = require('next-images')
+//const withImages = require('next-images')
 
 module.exports =  {
+  /*
   ...withImages(),
     future: {
         webpack5: true,
-    },
+    },*/
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.node = {
